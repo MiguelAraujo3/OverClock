dfc91eb8bf5ba4e0654a076468dc34f35c2f0fe8
Leandro <leandro.lafurtado@gmail.com>
2026-05-31 17:33:18 -0300

    feat: Criação de rotas login e cadastro

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from app.pagina_inicial.routes import base_route
 from app.pagina_agendamento.routes import agendamento_route
+from app.pagina_login.routes import login_route
+from app.pagina_cadastro.routes import cadastro_route
 
 #Criando aplicação Flask e registrando rotas do modulo
 app = Flask(__name__)
@@ -8,6 +10,10 @@ app = Flask(__name__)
 app.register_blueprint(base_route)
 
 app.register_blueprint(agendamento_route)
+
+app.register_blueprint(login_route)
+
+app.register_blueprint(cadastro_route)
 
 if __name__ == "__main__":
     app.run(debug=True)
