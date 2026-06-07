f2b6a7f8f22549646da3ff444bb22d423754f098
Vitor Gabriel <vitorgsms06@gmail.com>
2026-06-07 13:50:10 -0300

    feat: integrar widget do Cal.com e injetar dados via Jinja2

--- app/pagina_agendamento/routes.py
+++ app/pagina_agendamento/routes.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-#Estrutura da rota da página inicial
+# Estrutura da rota da página inicial
 agendamento_route = Blueprint('agendamento', __name__)
 
 @agendamento_route.route('/agendamento')
 @login_required
 def agendamento():
-    return render_template('agendamento.html')
+    link_agenda = "barbearia-overclock-aihkmo"
+    nome_usuario = "Nome do Usuário"
+    email_usuario = "usuario@exemplo.com"
+    
+    return render_template('agendamento.html', link_cal=link_agenda, nome=nome_usuario, email=email_usuario)
