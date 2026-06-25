5b67a2339c83052c1f5feef5fee69ae6cc9ec846
Leandro <leandro.lafurtado@gmail.com>
2026-06-24 23:53:46 -0300

    feat: configuração de email pelo flask-mail

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
 
-#Criando aplicação Flask e registrando rotas do modulo
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+# Criando aplicação Flask e registrando rotas do modulo
 app = Flask(__name__)
 
-#Secret key
-app.secret_key = 'OVERCLOCKZANDO'
+# Pegando a Secret key do .env
+app.secret_key = os.getenv('SECRET_KEY')
+# CONFIGURAÇÕES DO FLASK-MAIL
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+# Pegando os dados de e-mail do .env
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+
+# Inicializa o Mail na aplicação
+mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login.login'
 login_manager.login_message = 'Faça login para acessar esta página.'
+
 from app.pagina_login import buscar_usuario_no_csv
 login_manager.user_loader(buscar_usuario_no_csv)
 
@@ -23,8 +41,6 @@ app.register_blueprint(base_route)
 app.register_blueprint(agendamento_route)
 app.register_blueprint(login_route)
 app.register_blueprint(cadastro_route)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
