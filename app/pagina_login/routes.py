483de827f3cdfafb74e78496ec500e2bcb6f4d5f
Miguel Araújo Linux <miguelaraujo16052008@gmail.com>
2026-06-25 23:46:31 -0300

    feat: adicionando rota, caso o usuario vá para overclock.com/ já logado ele é redirecionado para tela de agendamento

--- app/pagina_login/routes.py
+++ app/pagina_login/routes.py
@@ -71,7 +71,7 @@ def logout():
     session.pop('usuario_email', None)
     
     flash("Você saiu da sua conta com sucesso.")
-    return redirect(url_for('home.home'))
+    return redirect(url_for('login.login'))
 
 #EXCLUSÃO DE CONTA
 @login_route.route('/excluir_conta', methods=['POST'])
