4eb78ca326117faa8fa37812fa9c796b24f0ce9d
Leandro <leandro.lafurtado@gmail.com>
2026-06-30 20:14:20 -0300

    fix: adição de condições faltando para validar cadastro

--- app/pagina_cadastro/routes.py
+++ app/pagina_cadastro/routes.py
@@ -19,6 +19,28 @@ def cadastro():
         if not nome or not email or not telefone or not senha or not confirmar_senha:
             flash("Por favor, preencha todos os campos do formulário.", "error")
             return redirect(url_for('cadastro.cadastro'))
+        
+        # Verificando se tem número no nome.
+        for caractere in nome:
+            if caractere.isdigit():
+                flash("O nome não pode conter números. Por favor, digite um nome válido.", "error")
+                return redirect(url_for('cadastro.cadastro'))
+
+        for caractere in telefone:
+            if caractere.isalpha():
+                flash("O número não pode conter letras. Por favor digite um número válido", "error")
+                return redirect(url_for('cadastro.cadastro'))
+
+        # Padronizar telefone para ter exatamente 11 números
+        # Isso remove parênteses, espaços e traços, deixando apenas os números (ex: 83988887777)
+        telefone_limpo = ''.join(filter(str.isdigit, telefone))
+
+        if len(telefone_limpo) != 11:
+            flash("O telefone deve conter exatamente 11 números, incluindo o DDD.", "error")
+            return redirect(url_for('cadastro.cadastro'))
+        
+        # Devolve a variavel telefone agora padronizado
+        telefone = f"({telefone_limpo[:2]}) {telefone_limpo[2:7]}-{telefone_limpo[7:]}"
                
         if len(senha) < 8:
             flash("A senha deve ter pelo menos 8 caracteres. Tente novamente.", "error")
