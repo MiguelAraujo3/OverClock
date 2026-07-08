c4947fa12d011b962c26086b2bfdc58aba8bbbd3
Miguel Araújo Linux <miguelaraujo16052008@gmail.com>
2026-07-07 22:09:16 -0300

    chore: excluindo alguns arquivos inutilizados

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-#Modulo usado para rodar a aplicação em produção
-import runpy
-
-# Força o Python a ler o arquivo físico app.py, ignorando a pasta de mesmo nome
-contexto = runpy.run_path("app.py")
-app = contexto["app"]
